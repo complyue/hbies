e3dc0dea607d921b710ef1d4c2908e8606d3e839
ComplYue <compl.yue@icloud.com>
2016-03-16 13:50:11 +0800

    wire close event

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const CONSTS = require('./lib/constants')
 
 module.exports.PACK_EVENT = CONSTS.HBI_PACK_EVENT
+module.exports.WIRE_CLOSE_EVENT = CONSTS.HBI_WIRE_CLOSE_EVENT
 module.exports.WIRE_ERR_EVENT = CONSTS.HBI_WIRE_ERR_EVENT
 module.exports.PEER_ERR_EVENT = CONSTS.HBI_PEER_ERR_EVENT
 module.exports.LANDING_ERR_EVENT = CONSTS.HBI_LANDING_ERR_EVENT
